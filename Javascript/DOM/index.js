cb749e85aa69cdbd84226fdac7327898e8f4fee2
Dolon Mim <mimbadhon@gmail.com>
2023-07-24 21:49:35 +0600

    DOM

---
+++ Javascript/DOM/index.js
@@ -0,0 +1,8 @@
+document.getElementsByTagName("li")[2].style.color="purple";
+document.getElementsByTagName("li").length;
+
+document.querySelector("li a").style.color="red";
+document.querySelector("#second").style.color="green";
+
+document.querySelector("button").style.background="yellow";
+

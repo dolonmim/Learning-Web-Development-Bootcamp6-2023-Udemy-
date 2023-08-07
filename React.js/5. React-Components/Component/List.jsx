d7128d923150a8a611279af664cb5f21701c55ce
Dolon Mim <mimbadhon@gmail.com>
2023-08-07 19:48:41 +0600

    React

---
+++ React.js/5. React-Components/Component/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function List()
+{
+  return <ul>
+    <li>Mango</li>
+    <li>Apple</li>
+  </ul>;
+
+}
+
+export default List;
---
+++ React.js/6. ES6-Calculator/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as cal from "./calculator";
+
+
+ReactDOM.render(
+  <div>
+    <li>{cal.add(2,3)}</li>
+    <li>{cal.sub(5,3)}</li>
+    <li>{cal.mul(5,10)}</li>
+    <li>{cal.div(30,5)}</li>
+    </div>
+,
+document.getElementById("root"));
